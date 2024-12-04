7ec463f3dd9f7ff22f0881d26e134841b25ad4a5
raj070907 <rajkumar_btechedit2023@kusoed.edu.np>
2024-12-04 11:57:57 +0545

    Python Assignment

---
+++ DATATYPE.py
@@ -0,0 +1,15 @@
+int_var = 50
+float_var = 27.89
+string = "Nepal is beautiful."
+dictionary = {"name": "Raj", "age": 18, "address": "Jorpati"}
+
+print(30 + 20 - int_var)
+print(100 / float_var)
+print("Hello World! " + string + " I live here.")
+print(dictionary)
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
